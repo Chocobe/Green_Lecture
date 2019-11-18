b3f81bccfd9e697ec9c52cdce828d4642b0e3f21
Chocobe <nice_10bird@naver.com>
2019-11-18 10:43:18 +0900

    <master> : Created BoardVO

---
+++ JSP/Exam02/src/com/board/BoardVO.java
@@ -0,0 +1,104 @@
+package com.board;
+
+public class BoardVO {
+	private int idx;
+	private String password;
+	private String name;
+	private String email;
+	private String homepage;
+	private String subject;
+	private String memo;
+	private String upload_1;
+	private String upload_2;
+	
+	private int watch;
+	private int good;
+	
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getHomepage() {
+		return homepage;
+	}
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+	
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+	
+	public String getUpload_1() {
+		return upload_1;
+	}
+	public void setUpload_1(String upload_1) {
+		this.upload_1 = upload_1;
+	}
+	
+	
+	public String getUpload_2() {
+		return upload_2;
+	}
+	public void setUpload_2(String upload_2) {
+		this.upload_2 = upload_2;
+	}
+	
+	
+	public int getWatch() {
+		return watch;
+	}
+	public void setWatch(int watch) {
+		this.watch = watch;
+	}
+	
+	
+	public int getGood() {
+		return good;
+	}
+	public void setGood(int good) {
+		this.good = good;
+	}
+}
